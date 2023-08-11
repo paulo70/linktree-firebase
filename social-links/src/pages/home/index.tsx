71a9593456ad6ff5685aba3a30f4a9522193a0a9
paulo70 <paulofrontend@gmail.com>
2023-08-11 18:09:51 -0300

    add networks page

--- social-links/src/pages/home/index.tsx
+++ social-links/src/pages/home/index.tsx
@@ -1,33 +1,116 @@
+import { useEffect, useState } from "react";
 import { Social } from "../../components/Social";
 import {FaLinkedinIn, FaInstagram, FaGithub} from "react-icons/fa"
 
+import {db} from "../../services/firebaseConnection"
+import {
+    getDocs,
+    collection,
+    orderBy,
+    query,
+    doc,
+    getDoc
+} from "firebase/firestore"
+import { list } from "firebase/storage";
+import { Link } from "react-router-dom";
+
+interface LinksProps{
+    id: string;
+    name: string;
+    url: string;
+    bg: string;
+    color: string;
+}
+
+interface SocialProps{
+    linkedin: string;
+    gitHub: string;
+    instagram: string;
+}
+
 export function Home(){
+    const [links, setLinks] = useState<LinksProps[]>([])
+    const [socialLink, setSocialLink] = useState<SocialProps>()
+
+    useEffect(() => {
+        function getLinks(){
+            const linkRef = collection(db, "links")
+            const queryRef = query(linkRef, orderBy("created", "asc"))
+            getDocs(queryRef)
+            .then((snapshot) => {
+               let list = [] as LinksProps[]
+
+               snapshot.forEach((doc) => {
+                 list.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    bg: doc.data().bg,
+                    color: doc.data().color,
+                    url: doc.data().url
+                 })
+               })
+               setLinks(list)
+            })
+            
+        }
+
+        getLinks()
+    },[])
+
+    useEffect(() => {
+        function loadingSocial(){
+            const docRef = doc(db, "links", "social")
+            getDoc(docRef)
+            .then((snapshot) => {
+                if(snapshot.data() !== undefined){
+                    setSocialLink({
+                        linkedin: snapshot.data()?.linkedin,
+                        instagram: snapshot.data()?.instagram,
+                        gitHub: snapshot.data()?.gitHub
+
+                    })
+                }
+            })
+        }
+
+        loadingSocial()
+    },[])
+
     return (
         <div className="flex flex-col w-full py-4 items-center justify-center">
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Paulo Costa - Front-End Developer</h1>
             <span className="text-gray-50 mb-5 mt-3">Social Medias</span>
 
             <main className="flex flex-col w-11/12 max-w-xl text-center">
-                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-                    <a>
-                        <p className="text-base md:text-lg">
-                            Canal Youtube
-                        </p>
-                    </a>
-                </section>
+                {links.map((link) => (
+                     <section
+                     style={{backgroundColor: link.bg}} 
+                     key={link.id} 
+                     className={`${link.bg} mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer`}>
+                        <Link to={link.url} target="_blank">
+                            <p className="text-base md:text-lg" style={{color: link.color}}>
+                                {link.name}
+                            </p>
+                        </Link>
+                    </section>
+                ))}
+
+               
             </main>
 
-            <footer className="flex justify-center gap-3 my-4">
-                <Social url="http://">
-                 <FaLinkedinIn size={35} color="#fff"/>
-                </Social>
-                 <Social url="http://">
-                 <FaInstagram size={35} color="#fff"/>
-                </Social>
-                 <Social url="http://">
-                 <FaGithub size={35} color="#fff"/>
-                </Social>
-            </footer>
+            {socialLink && Object.keys(setSocialLink).length > 0 && (
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url={socialLink?.linkedin}>
+                        <FaLinkedinIn size={35} color="#fff"/>
+                    </Social>
+                    <Social url={socialLink?.instagram}>
+                        <FaInstagram size={35} color="#fff"/>
+                    </Social>
+                    <Social url={socialLink?.gitHub}>
+                        <FaGithub size={35} color="#fff"/>
+                    </Social>
+                </footer>
+            )}
         </div>
     )
 }
--- social-links/src/pages/networks/index.tsx
+++ social-links/src/pages/networks/index.tsx
@@ -1,5 +1,83 @@
+import { FormEvent, useEffect, useState } from "react";
+import { Input } from "../../components/Input";
+import { Header } from "../../components/header";
+
+import {db} from "../../services/firebaseConnection"
+import {setDoc, doc, getDoc} from "firebase/firestore"
+
 export function NetWork(){
+    const [linkedin, setLinkedin] = useState("")
+    const [instagram, setInstagram] = useState("")
+    const [gitHub, setGitHub] = useState("")
+
+    useEffect(() => {
+     
+     function loadingLinks(){
+        const docRef = doc(db, "social", "links")
+        getDoc(docRef)
+        .then((snapshot) => {
+            if(snapshot.data() !== undefined){
+                setLinkedin(snapshot.data()?.linkedin)
+                setInstagram(snapshot.data()?.instagram)
+                setGitHub(snapshot.data()?.gitHub)
+            }
+        })
+     }
+
+     loadingLinks()
+
+    },[])
+
+    function handleRegister(e: FormEvent){
+        e.preventDefault()
+
+        setDoc(doc(db, "social", "links"), {
+            linkedin: linkedin,
+            instagram: instagram,
+            gitHub: gitHub,
+        })
+        .then(() => {
+            setLinkedin("")
+            setInstagram("")
+            setGitHub("")
+        })
+    }
+
     return (
-        <h1>NetWork</h1>
+        <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+            <Header/>
+            <h1 className="text-white text-2xl font-medium mt-8 mb-4">My NetWorks</h1>
+            <form onSubmit={handleRegister} className="flex flex-col max-w-xl w-full">
+                <label className="text-white font-medium mt-2 mb-2">Link Likedin</label>
+                <Input 
+                    type="url" 
+                    placeholder="type url Linkedin"
+                    value={linkedin}
+                    onChange={(e) => setLinkedin(e.target.value)}
+                />
+                <label className="text-white font-medium mt-2 mb-2">Link Instagram</label>
+                 <Input 
+                    type="url" 
+                    placeholder="type url Instagram"
+                    value={instagram}
+                    onChange={(e) => setInstagram(e.target.value)}
+                />
+
+                <label className="text-white font-medium mt-2 mb-2">Link GitHub</label>    
+                <Input 
+                    type="url" 
+                    placeholder="type url GitHub"
+                    value={gitHub}
+                    onChange={(e) => setGitHub(e.target.value)}
+                />
+
+                <button 
+                    type="submit"
+                    className="text-white bg-blue-600 h-9 rounded-md items-center justify-center flex mb-7"
+                    >
+                    Salvar Links
+                </button>
+            </form>
+        </div>
     )
 }
